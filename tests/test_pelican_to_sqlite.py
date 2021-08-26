4c068549570349c5aea27654eb8886ccaa6eb947
Ryan Cheley <rcheley@gmail.com>
2021-08-25 18:57:39 -0700

    added tests

--- tests/test_pelican_to_sqlite.py
+++ tests/test_pelican_to_sqlite.py
@@ -1,5 +1,16 @@
 import pytest
+from pelican_to_sqlite.cli import _helper
+from pelican_to_sqlite.settings import PELICAN_METADATA
 
 
-def test_placeholder():
-    pass
+def test_helper_true():
+    metadata = PELICAN_METADATA[0]
+    line = 'Title: This is my title'
+    test = _helper(metadata, line)
+    assert test
+
+def test_helper_false():
+    metadata = PELICAN_METADATA[0]
+    line = 'Not_in_metadata: This is my title'
+    test = _helper(metadata, line)
+    assert not test
