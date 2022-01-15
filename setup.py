5ec5214a9a484b6488e7271d54f303360119df07
Ryan Cheley <rcheley@gmail.com>
2022-01-15 09:54:15 -0800

    bump: version number

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
+from setuptools import setup, find_packages
 import io
 import os
-
-from setuptools import find_packages, setup
 
 VERSION = "0.2.0"
 
@@ -24,7 +23,8 @@ setup(
     license="Apache License, Version 2.0",
     packages=find_packages(),
     install_requires=["sqlite-utils", "click"],
-    extras_require={"test": ["pytest", "black", "isort", "coverage", "mypy", "cogapp"]},
+    extras_require={"test": ["pytest"]},
+    tests_require=["pelican-to-sqlite[test]"],
     setup_requires=["pytest-runner"],
     entry_points="""
         [console_scripts]
@@ -37,13 +37,4 @@ setup(
         "Changelog": "https://github.com/ryancheley/pelican-to-sqlite/releases",
     },
     python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Utilities",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
 )
