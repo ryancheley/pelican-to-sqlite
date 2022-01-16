e2bd8148d4e7f62e29e4dec3b938041d76fdd165
Ryan Cheley <rcheley@gmail.com>
2022-01-16 14:12:53 -0800

    feature: changed html content to text

--- pelican/plugins/pelican_to_sqlite/pelican_to_sqlite.py
+++ pelican/plugins/pelican_to_sqlite/pelican_to_sqlite.py
@@ -1,4 +1,5 @@
 import sqlite_utils
+import html2text
 
 from pelican import signals
 
@@ -7,27 +8,42 @@ DATABASE = "pelican.db"
 db = sqlite_utils.Database(DATABASE)
 
 
-def save_items(record: dict, table: str, db: sqlite_utils.Database) -> None:
+def save_items(record: dict, table: str, db: sqlite_utils.Database) -> None: # pragma: no cover
     db[table].insert(record, pk="slug", alter=True, replace=True)
 
 
-# Hook function, with the right parameters
-def run(_, content):
-    if content.status == "published":
+def create_record(content) -> dict:
+    record = {}
+    author = content.author.name
+    category = content.category.name
+    post_content = html2text.html2text(content.content)
+    published_date = content.date.strftime("%Y-%m-%d")
+    slug = content.slug
+    summary = html2text.html2text(content.summary)
+    title = content.title
+    url = "https://www.ryancheley.com/" + content.url
+    status = content.status
+    if status == "published" and title == "Vin's Last Game":
         record = {
-            "author": content.author.name,
-            "category": content.category.name,
-            "content": content.content,
-            "published_date": content.date.strftime("%Y-%m-%d"),
-            "slug": content.slug,
-            "summary": content.summary,
-            "title": content.title,
-            "url": "https://www.ryancheley.com/" + content.url,
+            "author": author,
+            "category": category,
+            "content": post_content,
+            "published_date": published_date,
+            "slug": slug,
+            "summary": summary,
+            "title": title,
+            "url": url,
         }
-        save_items(record, "content", db)
+    return record
+
+
+# Hook function, with the right parameters
+def run(_, content): # pragma: no cover
+    record = create_record(content)
+    save_items(record, "content", db)
 
 
 # Module entry point
-def register():
+def register(): # pragma: no cover
     # Connect the run hook function to the content_written signal
     signals.article_generator_write_article.connect(run)
